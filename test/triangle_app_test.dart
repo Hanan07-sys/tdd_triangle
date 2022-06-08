27b6c8d9bedd53ec2034a5e2a0451e59e118fccd
Hanan <hananababil07@gmail.com>
2022-06-08 22:47:35 +0800

    Initial commit

---
+++ test/triangle_app_test.dart
@@ -0,0 +1,27 @@
+//Triangle_app_test.dart
+import 'package:flutter_test/flutter_test.dart';
+import 'package:triangle/triangle_app.dart';
+
+void main() {
+  group('Detect the triangle', () {
+    test('Should throw Error when there is side less than 1', () {
+      expect(() => detectTriangle(-1, 2, 2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(1, -2, 2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(1, 2, -2), throwsA(isA<Exception>()));
+    });
+    test('Should return "Segitiga Sama Sisi" when all sides are equal', () {
+      expect(detectTriangle(1, 1, 1), "Segitiga Sama Sisi");
+      expect(detectTriangle(1, 1, 2), isNot("Segitiga Sama Sisi"));
+    });
+    test('Should return "Segitiga Sama Kaki" when only two sides are equal', () {
+      expect(detectTriangle(1, 1, 2), "Segitiga Sama Kaki");
+      expect(detectTriangle(1, 2, 1), "Segitiga Sama Kaki");
+      expect(detectTriangle(1, 2, 2), "Segitiga Sama Kaki");
+      expect(detectTriangle(4, 1, 4), "Segitiga Sama Kaki");
+    });
+    test('Should return "Segitiga Sembarang" when no sides are equal', () {
+      expect(detectTriangle(1, 2, 3), "Segitiga Sembarang");
+    });
+
+  });
+}
